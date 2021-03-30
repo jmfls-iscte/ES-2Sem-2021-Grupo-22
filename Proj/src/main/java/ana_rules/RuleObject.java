7d7f15c3451be8036cf7db10a3b83a84928d67d4
anantunes0 <atcas1@iscte-iul.pt>
2021-03-30 10:56:16 +0100

    ana's commit1

--- Proj/src/main/java/ana_rules/RuleObject.java
+++ Proj/src/main/java/ana_rules/RuleObject.java
@@ -2,11 +2,15 @@ package ana_rules;
 
 public class RuleObject {
 	public String info;
-	public String label; //Tipos de labels: name, metric, comparator, threshold, logic_operator
+	public String label; //Tipos de labels: name, metric, comparator, threshold, logic_operator 
+	
+	//criar enumerado com os tipos de label na interface, para ser acessivel a todas as classes e 
+	//nao existir repetiçao de codigo
+	
 	
 	public RuleObject(String info, String label){ 
 		this.info = info;
-		this.label = label;
+		this.label = label; 
 	}
 	
 	public String get_info() {
