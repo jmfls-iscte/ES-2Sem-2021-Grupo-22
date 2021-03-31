122bafb5b80858ef0e919bc5c1ca930af05e4dad
gcmfe <gcmfe@J4NRCX04V05216A>
2021-04-01 00:17:31 +0100

    JavaParser lê ficheiro Class +2 atributos Method +2 atributos ClassParser MethodParser POM dependência desnecessária

---
+++ Proj/src/main/java/metrics/JavaParser.java
@@ -0,0 +1,20 @@
+package metrics;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+
+public class JavaParser {
+
+	private static final String FILE_PATH = "src/main/java/metrics/Example.java";
+
+	public static void main(String[] args) throws FileNotFoundException {
+		CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
+		VoidVisitor<Void> classvisitor = new ClassParser();
+		((ClassParser) classvisitor).setCu(cu);
+		classvisitor.visit(cu, null);
+	}
+
+}
