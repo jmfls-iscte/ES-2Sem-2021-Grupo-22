9e909af3bec1549e147f2b569c263ffa99234814
Tiago Alves <tsasa@iscte.iul.pt>
2021-04-04 14:56:39 +0100

    Para além de salvar as regras no ficheiro, tam´bem já as lê e retorna um ArrayList<Rule>

--- Proj/src/main/java/ana_rules/SaveLoadRule.java
+++ Proj/src/main/java/ana_rules/SaveLoadRule.java
@@ -5,19 +5,42 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
+
+import gui.MainGui;
 
 public class SaveLoadRule {
 
-	ArrayList<Rule> rules;
+	private ArrayList<Rule> rules;
+	private String pathFile ;
+	
+	
+	public static void main(String[] args) {
+		try {
+			SaveLoadRule SLR = new SaveLoadRule();
+			ArrayList<Rule> rules3 = SLR.LoadRules();
+			
+			System.out.println("So para ler");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-	public SaveLoadRule() {
+	
+
+	public SaveLoadRule() throws IOException {
 		rules = new ArrayList<Rule>();
+		pathFile = "C:\\Users\\Tiago\\Desktop\\ES_PROJECT\\RuleClass.txt";
+		
 	}
 
 	public SaveLoadRule(ArrayList<Rule> rules) {
 		this.rules = rules;
+		pathFile = "C:\\Users\\Tiago\\Desktop\\ES_PROJECT\\RuleClass.txt";
 	}
 
 	
@@ -31,37 +54,81 @@ public class SaveLoadRule {
 	 */
 	public void SaveRules() {
 		try {
-			FileWriter fileRulesClass = new FileWriter("C:\\Users\\Tiago\\Desktop\\ES_PROJECT\\RuleClass.txt");
-			FileWriter fileMethodClass = new FileWriter("C:\\Users\\Tiago\\Desktop\\ES_PROJECT\\RuleMethod.txt");
 			
+			FileWriter fileRules = new FileWriter(pathFile);
+			//FileWriterfileRulesClass = new FileWriter(pathFile);
+			//FileWriter fileRulesMethod = new FileWriter(pathFile);
 			
-
 			for (Rule r : rules) {
 				String rule_info_String = RuleToString(r.getInfo());
 				
-				if (r.getType().equals("Class")) 
+				fileRules.append(r.getName()+";"+r.getType()+rule_info_String);
+				fileRules.append("\n");
+				
+				/*if (r.getType().equals("Class")) 
 				{
 					fileRulesClass.append(r.getName()+";"+r.getType()+rule_info_String);
 					fileRulesClass.append("\n");
 					System.out.println("Escrito\n");
 				} else if (r.getType().equals("Method")) {
-					fileMethodClass.write(r.getName()+";"+r.getType()+rule_info_String);
-					fileMethodClass.write("\n");
+					fileRulesMethod.append(r.getName()+";"+r.getType()+rule_info_String);
+					fileRulesMethod.append("\n");
 					System.out.println("Escrito\n");
 				} else 
 				{ 
 					/// ERROR
-				}
+				}*/
 			}
 			
 			
-			fileRulesClass.close();
-			fileMethodClass.close();
+			//fileRulesClass.close();
+			//fileRulesMethod.close();
+			
+			fileRules.close();
 
 		} catch (IOException e) {
 			
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public ArrayList<Rule> LoadRules() {
+		ArrayList<Rule> rules2 = new ArrayList<Rule>();
+		
+		try {
+			File fileRules = new File(pathFile);
+			Scanner reader = new Scanner(fileRules);
+			
+			while (reader.hasNextLine()) 
+			{
+				ArrayList<RuleObject> rule_info = new ArrayList<RuleObject>();
+				String line = reader.nextLine();
+				String[] splited = line.split(";");
+				String name = splited[0];
+				String type = splited[1];
+				for(int i=2; i<splited.length;i++)
+				{
+					String[] partsOfRule = splited[i].split(":");
+					String label = partsOfRule[0];
+					String info = partsOfRule[1];
+					
+					RuleObject ro = new RuleObject(info, label);
+					rule_info.add(ro);
+				}
+				
+				Rule rule = new Rule(name, type, rule_info);
+				rules2.add(rule);
+				
+			}
+				
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return rules2;
+		
 	}
 	
 	
