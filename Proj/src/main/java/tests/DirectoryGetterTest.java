8ee4338d67dcf7b23b505383a0a8a84bb6dc1649
anantunes0 <atcas1@iscte-iul.pt>
2021-04-05 17:26:07 +0100

    final ana's tests

--- Proj/src/main/java/tests/DirectoryGetterTest.java
+++ Proj/src/main/java/tests/DirectoryGetterTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
@@ -11,55 +12,107 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import metrics.DirectoryGetter;
+import metrics.Package;
 
 class DirectoryGetterTest {
+	static DirectoryGetter dg1;
+	static DirectoryGetter dg2;
+	static DirectoryGetter dg3;
 
-	static String teste ;
-	static DirectoryGetter dg;
-	static File teste_file;
+
+	static File file;
+	static File working_directory_file;
+
 	static List<Package> lst;
+	static String test = "ana_test";
+	
+	static String working_directory = "/Users/anantunes0/eclipse-workspace_ee/tp3";
+	static String working_directory_2 = "/Users/anantunes0/ES_directory_test_2";
+	
+	private List<Package> packages_test= new ArrayList<Package>();
+
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception{
-		teste = "teste";
-		dg = new DirectoryGetter();
-		teste_file = new File("teste");
-		lst = new ArrayList<Package>();
+		dg1 = new DirectoryGetter();
+		dg2 = new DirectoryGetter();
+		dg3 = new DirectoryGetter();
 
-		//dg.createPackage("teste", teste_file, lst);
+
+		file = new File("ana_test");
+		working_directory_file = new File("/Users/anantunes0/eclipse-workspace_ee/tp3");
+
+		lst = new ArrayList<Package>();
+		
+		dg3.SetDir(working_directory_2);
+
+		
+
 	}
 	
 	@Test
-	final void SetDirTest() {
-		Assertions.assertNotNull(dg);		
-		//Assertions.assertNotNull(dg.baseFile);
+	final void getDirFailTest() {
+		Assertions.assertNotNull(dg1);			
+		Throwable exception = assertThrows(IllegalArgumentException.class, () -> dg1.SetDir(test));
+	    assertNotNull(exception);
+	}
+	
+	
+	@Test
+	final void getDirTest() { 
+		Assertions.assertNotNull(dg2);			
+		dg2.SetDir(working_directory);
+		assertNotNull(dg2.getBaseFile());
+		assertEquals(working_directory_file, dg2.getBaseFile());
+	}
+	/*
+	
+	@Test NAO CONSIGO TESTAR ESTAS DUAS FUNÇÕES
+	final void getBaseFileTestFail() {
+		Assertions.assertNull(dg1.getBaseFile()); //PROBLEMAAASSS ????????
+	}
+	
+	@Test
+	final void getsrcTestFail() {
+		assertNull(dg1.getsrc()); //PROBLEMAAASSS ????????
 		
-		//assertThrows();
+	}*/
+	
+	@Test
+	final void getPackageNumberTest() {
+		assertEquals(0, dg2.getPackageNumber());
 		
 	}
-
+	
+	@Test
+	final void getPackagesTest() {
+		assertEquals(packages_test, dg2.getPackages());
+		
+	}
+	
+	@Test
+	final void getPackageListTest() {
+		assertEquals(packages_test, dg2.getPackageList());		
+	}
+	
 	@Test
 	final void FindSrcTest() {
-	//	Assertions.assertNotNull(dg.src);
-		//assertThrows();
-		//if
+		dg2.FindSrc();
+		assertEquals("src", dg2.getsrc().getName());
 	}
 
 	@Test
-	final void FindPackagesTest() {
-		//Assertions.assertNotNull(dg.Packages);
-
+	final void FindSrcTestFail() {
+		 assertThrows(IllegalStateException.class, () -> dg3.FindSrc());
 	}
-
+	
 	@Test
 	final void createPackageTest() {
-		//assertEquals(lst, dg.createPackage("teste", teste_file, lst));
-	}
-	
-	@Test
-	final void getPackageList() {
+		dg2.FindPackages();
+		assertNotNull(dg2.getPackages());
+		
 		
 	}
-	
+
 	
 }
