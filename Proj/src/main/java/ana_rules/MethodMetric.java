9a4ac559a5f1e18c8cf2ecdc289629fca505e3a5
jose aquafonte <jmfls@iscte.pt>
2021-04-08 14:33:04 +0100

    detecao de code smells

---
+++ Proj/src/main/java/ana_rules/MethodMetric.java
@@ -0,0 +1,27 @@
+package ana_rules;
+
+import metrics.Method;
+
+public enum MethodMetric {
+	LOC_METHOD,
+	CYCLO_METHOD;
+	
+	public static int getMetric(MethodMetric metric,Method method ) {
+		if(metric.equals(MethodMetric.LOC_METHOD)) {
+			return method.getLOC_method();
+		}else if (metric.equals(MethodMetric.CYCLO_METHOD)) {
+			return method.getCYCLO_method();
+		}else {
+			return 0;
+		}
+	}
+	public static boolean IsValid(String input) {
+		try {
+			MethodMetric.valueOf(input);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+}
--- Proj/src/main/java/ana_rules/RuleObject.java
+++ Proj/src/main/java/ana_rules/RuleObject.java
@@ -1,24 +1,46 @@
 package ana_rules;
 
+import org.apache.commons.collections4.EnumerationUtils;
+
 public class RuleObject {
 	private String info;
-	private String label; //Tipos de labels: name, metric, comparator, threshold, logic_operator 
+	private RuleObjectType label; //Tipos de labels: name, metric, comparator, threshold, logic_operator 
 	
 	//criar enumerado com os tipos de label na interface, para ser acessivel a todas as classes e 
 	//nao existir repetiçao de codigo
 	
 	
-	public RuleObject(String info, String label){ 
-		this.info = info;
-		this.label = label; 
+	public RuleObject(String info, String label) throws IllegalArgumentException{ 
+		this.label = RuleObjectType.valueOf(label);
+		if(this.label.equals(RuleObjectType.THRESHOLD) && !isNumeric(info)) {
+			throw new IllegalArgumentException("invalid rule Object");
+		}else if(this.label.equals(RuleObjectType.LOGIC_OPERATOR) && !Logical_Operator.IsValid(info) ) {
+			throw new IllegalArgumentException("invalid rule Object");
+		}else if(this.label.equals(RuleObjectType.CLASSMETRIC) && !ClassMetric.IsValid(info)) {
+			throw new IllegalArgumentException("invalid rule Object");
+		}else if(this.label.equals(RuleObjectType.COMPARISON_OPERATOR) && !Comparator_Operator.IsValid(info)) {
+			throw new IllegalArgumentException("invalid rule Object");
+		}else if(this.label.equals(RuleObjectType.METHODMETRIC) && !MethodMetric.IsValid(info)) {
+			throw new IllegalArgumentException("invalid rule Object");
+		}
+		this.info = info;;
 	}
 	
 	public String getInfo() {
 		return info;
 	}
 	
-	public String getLabel() {
+	public RuleObjectType getLabel() {
 		return label;
+	}
+	
+	public static boolean isNumeric(String str) {
+		try {
+			Double.parseDouble(str);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 }
 
