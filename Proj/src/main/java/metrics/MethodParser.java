a34871c5eba771b2944e27deab1ab0470b14e79b
gcmfe <gcmfe@J4NRCX04V05216A>
2021-04-15 15:25:25 +0100

    MethodParser if

--- Proj/src/main/java/metrics/MethodParser.java
+++ Proj/src/main/java/metrics/MethodParser.java
@@ -1,5 +1,5 @@
 package metrics;
-//
+
 import java.util.ArrayList;
 import java.util.List;
 import com.github.javaparser.ast.CompilationUnit;
@@ -34,50 +34,56 @@ class MethodParser extends VoidVisitorAdapter<Void> {
 		methods.add(method);
 	}
 
-	private void loop(List<Statement> stmt) {
-		for(int i=0;i<stmt.size();i++) {
-			if(stmt.get(i).isWhileStmt()) {
-				CYCLO_method ++;
+	private void loop(List<Statement> stmt) throws java.lang.ClassCastException {
+		for (int i = 0; i < stmt.size(); i++) {
+			if (stmt.get(i).isWhileStmt()) {
+				CYCLO_method++;
 				Statement aux = stmt.get(i).asWhileStmt().getBody();
 				List<Statement> stmt2 = ((BlockStmt) aux).getStatements();
 				loop(stmt2);
 			}
-			if(stmt.get(i).isForStmt()) {
-				CYCLO_method ++;
+			if (stmt.get(i).isForStmt()) {
+				CYCLO_method++;
 				Statement aux = stmt.get(i).asForStmt().getBody();
 				List<Statement> stmt2 = ((BlockStmt) aux).getStatements();
 				loop(stmt2);
 			}
-			if(stmt.get(i).isForEachStmt()) {
-				CYCLO_method ++;
+			if (stmt.get(i).isForEachStmt()) {
+				CYCLO_method++;
 				Statement aux = stmt.get(i).asForEachStmt().getBody();
 				List<Statement> stmt2 = ((BlockStmt) aux).getStatements();
 				loop(stmt2);
 			}
-			if(stmt.get(i).isSwitchStmt()) {
+			if (stmt.get(i).isSwitchStmt()) {
 				List<SwitchEntry> aux = stmt.get(i).asSwitchStmt().getEntries();
 				ArrayList<Statement> stmt2 = new ArrayList<Statement>();
 				CYCLO_method += stmt.get(i).asSwitchStmt().getEntries().size();
-				for(int j=0;j<stmt.get(i).asSwitchStmt().getEntries().size();j++) {
-					for(int k=0;k<aux.get(j).getStatements().size();k++) {
+				for (int j = 0; j < stmt.get(i).asSwitchStmt().getEntries().size(); j++) {
+					for (int k = 0; k < aux.get(j).getStatements().size(); k++) {
 						stmt2.add(aux.get(j).getStatement(k));
 					}
 				}
 				loop(stmt2);
 			}
-			if(stmt.get(i).isIfStmt()) {
-				CYCLO_method ++;
+			if (stmt.get(i).isIfStmt()) {
+				CYCLO_method++;
 				Statement aux = stmt.get(i).asIfStmt().getThenStmt();
-				List<Statement> stmt2 = ((BlockStmt) aux).getStatements();
-				if(stmt.get(i).asIfStmt().hasElseBlock()) {
-					CYCLO_method ++;
-					Statement aux2 = stmt.get(i).asIfStmt().getElseStmt().get();
-					aux2.asBlockStmt().getStatements();
-					stmt2.addAll(aux2.asBlockStmt().getStatements());	
+				System.out.print(aux);
+				try {
+					List<Statement> stmt2 = ((BlockStmt) aux).getStatements();
+					if (stmt.get(i).asIfStmt().hasElseBlock()) {
+						CYCLO_method++;
+						Statement aux2 = stmt.get(i).asIfStmt().getElseStmt().get();
+						aux2.asBlockStmt().getStatements();
+						stmt2.addAll(aux2.asBlockStmt().getStatements());
+					}
+					loop(stmt2);
+				} catch (Exception e) {
+					System.out.print(false);
 				}
-				loop(stmt2);
 			}
 		}
+
 	}
 
 	public ArrayList<Method> getMethods() {
