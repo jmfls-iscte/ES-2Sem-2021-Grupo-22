bce1d0fa13731d9416e8ecc063b6eca46f088d1a
Tiago Alves <tsasa@iscte.iul.pt>
2021-04-18 20:26:47 +0100

    resolução de erro

--- Proj/src/main/java/CodeSmellDetectionEvaluator/CodeSmellDetectionEvaluator.java
+++ Proj/src/main/java/CodeSmellDetectionEvaluator/CodeSmellDetectionEvaluator.java
@@ -1,5 +1,10 @@
 package CodeSmellDetectionEvaluator;
 
+import java.util.List;
+import metrics.*;
+import metrics.Package;
+
+
 public class CodeSmellDetectionEvaluator {
 	
 	
@@ -9,8 +14,18 @@ public class CodeSmellDetectionEvaluator {
 	private int falseNegative;
 	private int totalDetetions;
 	
-	public CodeSmellDetectionEvaluator()
+	private List<Package> packagesDetection;
+	private List<Package> packagesExcel;
+	
+	public CodeSmellDetectionEvaluator(List<Package> packagesDetection,List<Package> packagesExcel)
 	{
+		this.packagesDetection=packagesDetection;
+		this.packagesExcel = packagesExcel;
+	}
+	
+	public List<PackageEvaluator> evaluateCodeSmellsDetection()
+	{
+		return null;
 		
 	}
 	
--- Proj/src/main/java/CodeSmellDetectionEvaluator/PackageEvaluator.java
+++ Proj/src/main/java/CodeSmellDetectionEvaluator/PackageEvaluator.java
@@ -1,5 +1,34 @@
 package CodeSmellDetectionEvaluator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PackageEvaluator {
 
+	private String name;
+	private List<ClassEvaluator> classelst;
+	
+	public PackageEvaluator()
+	{
+		classelst = new ArrayList<ClassEvaluator>();
+	}
+	
+	public PackageEvaluator(String name)
+	{
+		this.name = name;
+		classelst = new ArrayList<ClassEvaluator>();
+	}
+	
+	public PackageEvaluator(String name, List<ClassEvaluator> classelst)
+	{
+		this.name = name;
+		classelst = classelst;
+	}
+	
+	
+	public void addClass(ClassEvaluator classToAdd)
+	{
+		classelst.add(classToAdd);
+	}
+	
 }
