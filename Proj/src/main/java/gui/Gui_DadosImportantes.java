2a48e5fde3effa92fa6d2e8d86ad2b67a729b162
rmrsc <rmrsc@iscte-iul.pt>
2021-04-21 17:20:35 +0100

    commit

--- Proj/src/main/java/gui/Gui_DadosImportantes.java
+++ Proj/src/main/java/gui/Gui_DadosImportantes.java
@@ -1,5 +1,7 @@
 package gui;
 
+import java.util.ArrayList;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
@@ -9,6 +11,10 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.wb.swt.SWTResourceManager;
+
+import ana_rules.Rule;
+import ana_rules.RuleObject;
+
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 
@@ -18,6 +24,7 @@ public class Gui_DadosImportantes {
 
 	protected Shell shell;
 	private Text excelPath_txt;
+	private ArrayList<Rule> rules;
 
 	/**
 	 * Launch the application.
@@ -59,14 +66,12 @@ public class Gui_DadosImportantes {
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
 		
 		Label excelPath_lbl = new Label(shell, SWT.NONE);
 		GridData gd_excelPath_lbl = new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1);
 		gd_excelPath_lbl.widthHint = 117;
 		excelPath_lbl.setLayoutData(gd_excelPath_lbl);
-		excelPath_lbl.setFont(SWTResourceManager.getFont("Segoe UI", 13, SWT.NORMAL));
+		excelPath_lbl.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.NORMAL));
 		excelPath_lbl.setText("Excel Path :");
 		
 		excelPath_txt = new Text(shell, SWT.BORDER);
@@ -76,24 +81,62 @@ public class Gui_DadosImportantes {
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
 		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
-		new Label(shell, SWT.NONE);
 		
 		Button mostrarDadosImp_btn = new Button(shell, SWT.NONE);
 		
 		mostrarDadosImp_btn.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
-		mostrarDadosImp_btn.setFont(SWTResourceManager.getFont("Segoe UI", 13, SWT.NORMAL));
+		mostrarDadosImp_btn.setFont(SWTResourceManager.getFont("Segoe UI", 11, SWT.NORMAL));
 		mostrarDadosImp_btn.setText("Mostrar Dados Importados");
 		
 		
 		mostrarDadosImp_btn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDoubleClick(MouseEvent e) {
-				ExcelRead excel = new ExcelRead(excelPath_txt.getText());
+				
+				RuleObject obj1 = new RuleObject("LOC_METHOD", "METHODMETRIC");
+				RuleObject obj2 = new RuleObject("GREATER", "COMPARISON_OPERATOR");
+				RuleObject obj3 = new RuleObject("50", "THRESHOLD");
+				RuleObject obj4 = new RuleObject("AND", "LOGIC_OPERATOR");
+				RuleObject obj5 = new RuleObject("CYCLO_METHOD", "METHODMETRIC");
+				RuleObject obj6 = new RuleObject("GREATER", "COMPARISON_OPERATOR");
+				RuleObject obj7 = new RuleObject("10", "THRESHOLD");
+				ArrayList<RuleObject> ruleinfo = new ArrayList<RuleObject>();
+				ruleinfo.add(obj1);
+				ruleinfo.add(obj2);
+				ruleinfo.add(obj3);
+				ruleinfo.add(obj4);
+				ruleinfo.add(obj5);
+				ruleinfo.add(obj6);
+				ruleinfo.add(obj7);
+				
+				RuleObject obj21 = new RuleObject("WMC_CLASS", "METHODMETRIC");
+				RuleObject obj22 = new RuleObject("GREATER", "COMPARISON_OPERATOR");
+				RuleObject obj23 = new RuleObject("50", "THRESHOLD");
+				RuleObject obj24 = new RuleObject("OR", "LOGIC_OPERATOR");
+				RuleObject obj25 = new RuleObject("NOM_CLASS", "METHODMETRIC");
+				RuleObject obj26 = new RuleObject("GREATER", "COMPARISON_OPERATOR");
+				RuleObject obj27 = new RuleObject("10", "THRESHOLD");
+				ArrayList<RuleObject> ruleinfo2 = new ArrayList<RuleObject>();
+				ruleinfo2.add(obj21);
+				ruleinfo2.add(obj22);
+				ruleinfo2.add(obj23);
+				ruleinfo2.add(obj24);
+				ruleinfo2.add(obj25);
+				ruleinfo2.add(obj26);
+				ruleinfo2.add(obj27);
+				
+				try {
+					Rule longmethod = new Rule ("Is_Long_Method","method",ruleinfo,true);
+					Rule godclass = new Rule ("Is_God_Class","class",ruleinfo2,true);
+					rules.add(longmethod);
+					rules.add(godclass);
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				
+				ExcelRead excel = new ExcelRead(excelPath_txt.getText(),rules);
 				excel.ReadFile();
 				System.out.println("hhh");
 				
